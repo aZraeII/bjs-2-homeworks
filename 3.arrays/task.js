4c59378887bedaf73580c324fe83bce950f7248e
aZraeII <kolya-@bk.ru>
2023-01-02 16:04:12 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,15 @@
 function compareArrays(arr1, arr2) {
-  
+  let comparison = arr1.length === arr2.length && arr1.every((item, array) => item === arr2[array]);
+  return comparison;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let genderOfAPerson = users.filter(item => item.gender === gender);
+  let searchAge = genderOfAPerson.map(item => item.age);
+  let sum = searchAge.reduce((acc, item) => acc + item, 0);
+  if (sum > 0) {
+    return (sum / searchAge.length);
+  } else {
+    return sum;
+  }
 }
