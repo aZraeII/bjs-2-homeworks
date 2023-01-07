b9fb4488d54cde046d093ed3b2d3312905d3b453
aZraeII <kolya-@bk.ru>
2023-01-08 00:03:15 +0300

    initial commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,13 +1,19 @@
 function Student(name, gender, age) {
-  
-}
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
+  }
+
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.setSubject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if('marks' in Student === true) {
+    this.addMarks = [...marks];
+  }
 }
 
 Student.prototype.getAverage = function () {
