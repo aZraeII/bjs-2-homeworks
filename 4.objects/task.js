5982f6ddb974bd57d8d477efcefdd82e0df1d9d8
aZraeII <kolya-@bk.ru>
2023-01-09 01:26:39 +0300

    делаю

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,3 +1,4 @@
+"use strict"
 function Student(name, gender, age) {
   this.name = name;
   this.gender = gender;
@@ -5,21 +6,28 @@ function Student(name, gender, age) {
   this.marks = [];
   }
 
-
 Student.prototype.setSubject = function (subjectName) {
-    this.setSubject = subjectName;
+    student.setSubject = subjectName;
+    return student.setSubject
 }
 
 Student.prototype.addMarks = function (...marks) {
-  if('marks' in Student === true) {
-    this.addMarks = [...marks];
+  if('marks' in student === true) {
+    student.marks = [...marks];
   }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if('marks' in student === true && student.marks.length > 0) {
+    let sum = student.marks.reduce((acc, item) => acc + item, 0);
+    return (sum / student.marks.length);
+  } else {
+    return 0;
+  }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete student.marks;
+  delete student.subject;
+  this.excluded = reason;
 }
