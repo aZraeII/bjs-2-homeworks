6234698b3c3a7c4b059c3933e799e7c9eaff6e8c
aZraeII <kolya-@bk.ru>
2023-01-19 00:44:42 +0300

    Пятое задание полностью готово

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -22,14 +22,12 @@ class PrintEditionItem{
         return this._state;
     }
 }
-
 class Magazine extends PrintEditionItem{
     constructor(name, releaseDate, pagesCount){
         super(name, releaseDate, pagesCount);
         this.type = "magazine";
     }
 }
-
 class Book extends PrintEditionItem{
     constructor(author, name, releaseDate, pagesCount){
         super(name, releaseDate, pagesCount);
@@ -37,28 +35,24 @@ class Book extends PrintEditionItem{
         this.author = author;
     }
 }
-  
 class NovelBook extends Book{
     constructor(author, name, releaseDate, pagesCount){
         super(author, name, releaseDate, pagesCount);
         this.type = "novel";
     }    
 }
-
 class FantasticBook extends Book{
     constructor(author, name, releaseDate, pagesCount){
         super(author, name, releaseDate, pagesCount);
         this.type = "fantastic";
     }    
 }
-
 class DetectiveBook extends Book{
     constructor(author, name, releaseDate, pagesCount){
         super(author, name, releaseDate, pagesCount);
         this.type = "detective";
     }   
 }
-
 class Library{
     constructor(name, books){
         this.name = name;
@@ -80,29 +74,3 @@ class Library{
         return book;
     }
 }
-
-
-
-const library = new Library("Библиотека имени Ленина");
-
-library.addBook(
- new DetectiveBook(
-   "Артур Конан Дойл",
-   "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
-   2019,
-   1008
- )
-);
-library.addBook(
- new FantasticBook(
-   "Аркадий и Борис Стругацкие",
-   "Пикник на обочине",
-   1972,
-   168
- )
-);
-library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138));
-library.addBook(new Magazine("Мурзилка", 1924, 60));
-console.log(library.findBookBy("name", "Властелин колец")); //null
-console.log(library.findBookBy("releaseDate", 1924).name); //"Мурзилка"
-
