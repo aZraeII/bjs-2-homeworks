df77c33adba212bc68f1ec58c22a9d4bc63de823
aZraeII <kolya-@bk.ru>
2023-01-21 20:27:21 +0300

    на работе 2 задание начал

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -8,7 +8,6 @@
     }  
 }
 
-
 function validateCount(item){
     try{
        return parseCount(item);
@@ -16,3 +15,27 @@ function validateCount(item){
         return err;
     }
 }
+
+class Triangle{
+    constructor(a, b, c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        if(a >= b + c || b >= a + c || c >= b + a){
+            throw new Error('Треугольник с такими сторонами не существует');
+        }
+    }
+    get perimeter(){
+        return (a + b + c);
+    }
+    get area(){
+        let p = (a + b + c) / 2;
+        let s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        return +s.toFixed(3)
+    }
+}
+
+function getTriangle(a, b ,c){
+    return new Triangle
+}
+
