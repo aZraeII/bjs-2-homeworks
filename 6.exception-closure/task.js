bd1758d5c90c36b7da0e0c65a81d41affc1e5c7e
aZraeII <kolya-@bk.ru>
2023-01-21 18:22:33 +0300

    initial commit

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,9 @@
-﻿
+﻿function parseCount(item){
+    if(typeof(item) === Number){
+        console.log(item);
+    }else if(typeof(item) === String){
+        console.log(Number.parseFloat(item));
+    }else if(parseInt(item) === NaN){
+        throw new Error('Невалидное значение');
+    }
+}
