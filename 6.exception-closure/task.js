0a2ae49896a0b8f9125d9b339865898daf242bcd
aZraeII <kolya-@bk.ru>
2023-01-23 19:22:21 +0300

    Шестое задание полностью готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -16,26 +16,37 @@ function validateCount(item){
     }
 }
 
+
 class Triangle{
     constructor(a, b, c){
         this.a = a;
         this.b = b;
         this.c = c;
-        if(a >= b + c || b >= a + c || c >= b + a){
+        if((a >= b + c) || (b >= a + c) || (c >= b + a)){
             throw new Error('Треугольник с такими сторонами не существует');
         }
     }
     get perimeter(){
-        return (a + b + c);
+        return (this.a + this.b + this.c);
     }
     get area(){
-        let p = (a + b + c) / 2;
-        let s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        let p = (this.a + this.b + this.c) / 2;
+        let s = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
         return +s.toFixed(3)
     }
 }
 
 function getTriangle(a, b ,c){
-    return new Triangle
+    try{
+        return new Triangle(a, b ,c)
+    }catch(err){
+        return {
+            get perimeter(){
+                return 'Ошибка! Треугольник не существует';
+            },
+            get area(){
+                return 'Ошибка! Треугольник не существует';
+            }
+        }
+    }
 }
-
